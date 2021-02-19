80f58181edb51e897d2f53c81d22786d8049cac2
Christiano Donke <cdonke@outlook.com>
2021-02-19 19:16:48 -0300

    Cyclic Rotation

---
+++ Algorithms.Tests/Codility/Arrays/CyclicRotationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Codility.Arrays
+{
+    [TestClass]
+    public class CyclicRotationTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 3, 8, 9, 7, 6 }, 3, new int[] { 9, 7, 6, 3, 8 })]
+        [DataRow(new int[] { 0, 0, 0 }, 1, new int[] { 0, 0, 0 })]
+        [DataRow(new int[] { 1, 2, 3, 4 }, 4, new int[] { 1, 2, 3, 4 })]
+        public void FirstTry(int[] A, int K, int[] expected)
+        {
+            var solution = new Algorithms.Codility.Arrays.CyclicRotation.Solution();
+            var actual = solution.FirstTry(A, K);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Codility/Arrays/CyclicRotation/Solution.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Codility.Arrays.CyclicRotation
+{
+    [MemoryDiagnoser]
+    public class Solution
+    {
+        [Benchmark]
+        [Arguments(new int[] { 3, 8, 9, 7, 6 }, 3)]
+        [Arguments(new int[] { 0, 0, 0 }, 1)]
+        [Arguments(new int[] { 1, 2, 3, 4 }, 4)]
+        public int[] FirstTry(int[] A, int K)
+        {
+            if (A.Length > 100 || A.Length < 0)
+                throw new OutOfMemoryException(nameof(A));
+            if (K > 100 || K < 0)
+                throw new OutOfMemoryException(nameof(K));
+
+            var result = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+                result[(i + K) % A.Length] = A[i];
+
+            return result;
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 3, 8, 9, 7, 6 }, 3)]
+        [Arguments(new int[] { 0, 0, 0 }, 1)]
+        [Arguments(new int[] { 1, 2, 3, 4 }, 4)]
+        public int[] SecondTry(int[] A, int K)
+        {
+            if (A.Length > 100 || A.Length < 0)
+                throw new OutOfMemoryException(nameof(A));
+            if (K > 100 || K < 0)
+                throw new OutOfMemoryException(nameof(K));
+
+            var result = new int[A.Length];
+            int nextPosition = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                nextPosition = (i + K) % A.Length;
+                result[nextPosition] = A[i];
+            }
+
+            return result;
+        }
+    }
+}
