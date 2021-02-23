bc9e9abaf3ee34aeffd2fac7cdaa5b65f94da304
Christiano Donke <cdonke@outlook.com>
2021-02-23 17:48:13 -0300

    FrogRiverOne

---
+++ Algorithms.Tests/Codility/CountingElements/FrogRiverOneTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Codility.CountingElements
+{
+    [TestClass]
+    public class FrogRiverOneTests
+    {
+        [TestMethod]
+        [DataRow(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6)]
+        [DataRow(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6)]
+        public void FirstTry(int X, int[] A, int expected)
+        {
+            var solution = new Algorithms.Codility.CountingElements.FrogRiverOne.FrogRiverOne();
+
+            var actual = solution.FirstTry(X, A);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Codility/CountingElements/FrogRiverOne/FrogRiverOne.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Codility.CountingElements.FrogRiverOne
+{
+    [MemoryDiagnoser]
+    public class FrogRiverOne
+    {
+        [Benchmark]
+        [Arguments(6, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 })]
+        public int FirstTry(int X, int[] A)
+        {
+            // Create an empty list for all the positions of leaves in the river
+            // IF they felt already, it must be true.
+            var path = new bool[X];
+
+            // Iterate through all the leaves falling
+            for (int i = 0; i < A.Length; i++)
+            {
+                // Leaves are 1-indexed, and not 0-index. This way, the current position in the array is P-1.
+                // If the current leaf hadn't fallen yet, set is as true.
+                if (!path[A[i] - 1])
+                {
+                    path[A[i] - 1] = true;
+
+                    // Decrement X for each placed leaf.
+                    // When X == 0, means that all the leaves are placed, and i indicates the time.
+                    if (--X == 0)
+                        return i;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
