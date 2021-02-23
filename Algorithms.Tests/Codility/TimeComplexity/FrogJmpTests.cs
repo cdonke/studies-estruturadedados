f8b4784db527eb12fb62f508ad7433508d681c2c
Christiano Donke <cdonke@outlook.com>
2021-02-23 15:14:00 -0300

    Frog Jump

---
+++ Algorithms.Tests/Codility/TimeComplexity/FrogJmpTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Codility.TimeComplexity
+{
+    [TestClass]
+    public class FrogJmpTests
+    {
+        [TestMethod]
+        [DataRow(10,85,30, 3)]
+        public void FirstTry(int X, int Y, int D, int expected)
+        {
+            var solution = new Algorithms.Codility.TimeComplexity.FrogJmp.FrogJmp();
+            var actual = solution.FirstTry(X, Y, D);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ Algorithms/Codility/TimeComplexity/FrogJmp/FrogJmp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Codility.TimeComplexity.FrogJmp
+{
+    public class FrogJmp
+    {
+        public int FirstTry(int X, int Y, int D)
+        {
+            return (int)Math.Ceiling((Y - X) / (double)D);
+        }
+    }
+}
