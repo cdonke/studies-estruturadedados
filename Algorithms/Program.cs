270f90bc7ed835cba61e72cf8281f38dcbce071b
Christiano Donke <cdonke@outlook.com>
2021-02-24 11:28:19 -0300

    Fix arguments to run benchmark

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -9,7 +9,7 @@ namespace Algorithms
         [ExcludeFromCodeCoverage]
         static void Main(string[] args)
         {
-            if (args.Length > 0 && args[0] == "--all")
+            if (args.Length > 0)
                 BenchmarkRunner.Run(typeof(Program).Assembly);
             else
                 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
