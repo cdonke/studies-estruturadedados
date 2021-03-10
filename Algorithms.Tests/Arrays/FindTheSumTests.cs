47b34edfc9b9d64317042ad17b6423d1fdb3cadf
Christiano Donke <cdonke@outlook.com>
2021-03-10 10:32:30 -0300

    Expandind tests- Find the Sum

--- Algorithms.Tests/Arrays/FindTheSumTests.cs
+++ Algorithms.Tests/Arrays/FindTheSumTests.cs
@@ -10,39 +10,42 @@ namespace Algorithms.Tests.Arrays
     {
         [TestMethod]
         [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
-        public void FirstTry(int[] A, int sum)
+        public void FirstTry(int[] A, int sum, bool expected)
         {
             var solution = new Algorithms.Arrays.FindTheSum.FindTheSum();
             var actual = solution.FirstTry(A, sum);
 
-            Assert.IsTrue(actual);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
-        public void SecondTry(int[] A, int sum)
+        public void SecondTry(int[] A, int sum, bool expected)
         {
             var solution = new Algorithms.Arrays.FindTheSum.FindTheSum();
             var actual = solution.SecondTry(A, sum);
 
-            Assert.IsTrue(actual);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
-        public void ThirdTry(int[] A, int sum)
+        public void ThirdTry(int[] A, int sum, bool expected)
         {
             var solution = new Algorithms.Arrays.FindTheSum.FindTheSum();
             var actual = solution.ThirdTry(A, sum);
 
-            Assert.IsTrue(actual);
+            Assert.AreEqual(expected, actual);
         }
 
         public static IEnumerable<object[]> Data()
         {
             var solution = new Algorithms.Arrays.FindTheSum.FindTheSum();
             foreach (object[] item in solution.Data())
-                yield return item;
+                yield return new object[] { item[0], item[1], true };
+
+            yield return new object[] { new int[] { 10, 5 }, 0, false };
+            yield return new object[] { new int[] { 1, 9, 19, 5 }, 10 , true };
         }
     }
 }
