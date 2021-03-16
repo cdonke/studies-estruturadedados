fb4c7e33f53fe7b34eba439c5aa6e8244dff1a50
Christiano Donke <cdonke@outlook.com>
2021-03-16 15:51:00 -0300

    Two Sum

---
+++ Algorithms.Tests/Leetcode/Easy/TwoSumTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Leetcode.Easy
+{
+    [TestClass]
+    public class TwoSumTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void FirstTry(int[] nums, int target, int[] expected)
+        {
+            var solution = new Algorithms.Leetcode.Easy.TwoSum();
+            var actual = solution.FirstTry(nums, target, null);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void SecondTry(int[] nums, int target, int[] expected)
+        {
+            var solution = new Algorithms.Leetcode.Easy.TwoSum();
+            var actual = solution.SecondTry(nums, target, null);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void ThirdTry(int[] nums, int target, int[] expected)
+        {
+            var solution = new Algorithms.Leetcode.Easy.TwoSum();
+            var actual = solution.ThirdTry(nums, target, null);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Data()
+        {
+            var solution = new Algorithms.Leetcode.Easy.TwoSum();
+            foreach (object[] item in solution.Data())
+                yield return item;
+        }
+    }
+}
---
+++ Algorithms/Leetcode/Easy/TwoSum.cs
@@ -0,0 +1,77 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Leetcode.Easy
+{
+    [MemoryDiagnoser, KeepBenchmarkFiles, MarkdownExporterAttribute.GitHub]
+    public class TwoSum
+    {
+        public IEnumerable<object[]> Data()
+        {
+            yield return new object[] { new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 } };
+            yield return new object[] { new[] { 3, 2, 4 }, 6, new[] { 1, 2 } };
+            yield return new object[] { new[] { 3, 3 }, 6, new[] { 0, 1 } };
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public int[] FirstTry(int[] nums, int target, int[] expected)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+                }
+            }
+
+            return new int[0];
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public int[] SecondTry(int[] nums, int target, int[] expected)
+        {
+            var map = new Hashtable(nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                map.Add(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement) && (int)map[complement] != i)
+                {
+                    return new int[] { i, (int)map[complement] };
+                }
+            }
+
+            throw new Exception("No solutons found");
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public int[] ThirdTry(int[] nums, int target, int[] expected)
+        {
+            var map = new Hashtable(nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement) && (int)map[complement] != i)
+                {
+                    return new int[] { (int)map[complement], i };
+                }
+
+                map.Add(nums[i], i);
+            }
+
+            throw new Exception("No solutons found");
+        }
+    }
+}
