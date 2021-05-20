8e5103ed3f3df9c021dfb2495162548a49a89952
Christiano Donke <cdonke@outlook.com>
2021-05-20 17:00:00 -0300

    Number of visible nodes

---
+++ Algorithms.Tests/Codility/Exams/NumberOfVisibleNodes.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Codility.Exams
+{
+    [TestClass]
+    public class NumberOfVisibleNodes
+    {
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void FirstTry(Algorithms.Codility.Exams.NumberOfVisibleNodes.NumberOfVisibleNodes.Tree Tree, int expected)
+        {
+            var solution = new Algorithms.Codility.Exams.NumberOfVisibleNodes.NumberOfVisibleNodes();
+            var actual = solution.FirstTry(Tree, 0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Data() =>
+            Algorithms.Codility.Exams.NumberOfVisibleNodes.NumberOfVisibleNodes.Data();
+    }
+}
---
+++ Algorithms/Codility/Exams/NumberOfVisibleNodes/NumberOfVisibleNodes.cs
@@ -0,0 +1,98 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Algorithms.Codility.Exams.NumberOfVisibleNodes
+{
+    [MemoryDiagnoser]
+    [KeepBenchmarkFiles]
+    [MarkdownExporterAttribute.GitHub]
+    public class NumberOfVisibleNodes
+    {
+        public class Tree
+        {
+            public int x;
+            public Tree left;
+            public Tree right;
+        }
+
+        [ExcludeFromCodeCoverage]
+        public static IEnumerable<object[]> Data()
+        {
+            yield return new object[] {
+                new Tree {
+                    x = 5,
+                    left = new Tree
+                    {
+                        x=3,
+                        left = new Tree
+                        {
+                            x = 20
+                        },
+                        right = new Tree
+                        {
+                            x = 21
+                        }
+                    },
+                    right = new Tree
+                    {
+                        x = 10,
+                        left = new Tree
+                        {
+                            x = 1
+                        }
+                    }
+                }
+                , 4
+            };
+
+            yield return new object[] {
+                new Tree {
+                    x = 8,
+                    left = new Tree
+                    {
+                        x=2,
+                        left = new Tree
+                        {
+                            x = 8
+                        },
+                        right = new Tree
+                        {
+                            x =7
+                        }
+                    },
+                    right = new Tree
+                    {
+                        x = 6                        
+                    }
+                },
+                2
+            };
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public int FirstTry(Tree T, int expected)
+        {
+            return countVisible(T, int.MinValue);
+        }
+        int countVisible(Tree T, int maxValue)
+        {
+            if (T == null)
+            {
+                return 0;
+            }
+
+            int num = 0;
+            if (T.x >= maxValue)
+            {
+                num = 1;
+                maxValue = T.x;
+            }
+
+            return num + countVisible(T.left, maxValue) + countVisible(T.right, maxValue);
+        }
+    }
+}
